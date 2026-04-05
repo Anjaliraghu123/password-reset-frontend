b9a73d3cab08dc56271c1e75053991352ca4aa0a
Anjaliraghu123 <anjalifromkpm@gmail.com>
2026-04-05 17:59:24 +0530

    changes

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = "https://password-reset-backend-gt25.onrender.com/api/auth";
 
 export const registerUser = (data) => axios.post(`${API_URL}/register`, data);
 export const loginUser = (data) => axios.post(`${API_URL}/login`, data);
